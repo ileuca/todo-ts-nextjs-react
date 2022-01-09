9e6f92dd95831d51eb498511ef4bb45a7aa93dbd
Ionel Leuca <ionel.leuca@gmail.com>
2022-01-09 11:45:21 +0200

    singleTask

--- pages/task/[id].tsx
+++ pages/task/[id].tsx
@@ -1,6 +1,5 @@
 import { Button, Input } from "@chakra-ui/react";
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react";
 import useTasks from "../../hooks/useTasks";
 import { Task } from "../../types/task";
 
@@ -9,14 +8,26 @@ const SingleTask = ()=>{
     const {tasks, updateTask} = useTasks();
     const initialtask = tasks?.find(task=>{return task.id === router.query.id});
 
+    const handleChange = (event:any) =>{
+        const updatedTask:Task ={
+            id: initialtask?.id,
+            title: event.target.value,
+            isDone: initialtask?.isDone
+        }
+        updateTask(updatedTask.id,updatedTask)
+    }
+
+    const handleClick =()=>{
+        window.location.href="/"
+    }
 
     return(
         <>
         <p>{initialtask?.id}</p>
         <p>{initialtask?.title}</p>
 
-        <Input value={initialtask?.title || ""} onChange={()=>{}} placeholder='Title' />
-        <Button onClick={()=>{}} colorScheme='blue'>Update Task</Button>
+        <Input value={initialtask?.title || ""} onChange={handleChange} placeholder='Title' />
+        <Button onClick={handleClick} colorScheme='blue'>Close</Button>
         </>
     )}
 
