63de65988b42f16b5b3113d8b5f70533ec98d1ef
Ionel Leuca <ionel.leuca@gmail.com>
2022-01-09 19:18:42 +0200

    Break-Spaces min suported WIth = 280 px

--- pages/tasks.tsx
+++ pages/tasks.tsx
@@ -28,7 +28,7 @@ return(
             <HStack key={task.id}>
                     <HStack >
                     <Checkbox size='lg' colorScheme={handleColor(task.isDone)} isChecked={task.isDone} onChange={()=> handleChange(task.id)}>
-                        <Button style={{maxWidth:"42vw", overflow:"hidden", whiteSpace:"nowrap", textOverflow:"ellipsis",display:"inline-block", textAlign:"center"}} 
+                        <Button style={{maxWidth:"34vw", overflow:"hidden", whiteSpace:"break-spaces", textOverflow:"ellipsis",display:"inline-block", textAlign:"center"}} 
                         colorScheme={handleColor(task.isDone)} onClick={()=>{handleChange(task.id)}}>
                             {task.title}
                             </Button>
